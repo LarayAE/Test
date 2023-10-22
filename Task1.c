7ab9d576fe21790b74b066814ca915658cbf3033
LarayAE <lae92@mail.ru>
2023-10-23 02:20:22 +0300

    implementation of element search

--- Task1.c
+++ Task1.c
@@ -1,11 +1,15 @@
-#include "stdint.h"
+#include <stddef.h>
 
 
 int32_t get_unique_num(int32_t *array, size_t len)
 {
 	int32_t res;
 
-	// Ваше решение...
+	// Для поиска не парного элемента массива за один проход используем XOR
+	for(size_t i = 0; i < len; i++)
+	{
+		res ^= array[i];
+	}
 
 	return res;
 }
